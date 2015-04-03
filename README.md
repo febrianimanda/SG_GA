b72029a399e86474f0c0b9b77c7952b50a9a5114
febrianimanda <febrian.imanda@gmail.com>
2015-04-04 00:57:27 +0700

    Cross over done

--- README.md
+++ README.md
@@ -4,4 +4,14 @@ By : Febrian Imanda Effendy, Fahmi, Aldi
 Memiliki populasi sebanyak 10 kromosom
 Di setiap kromosom terdapat 8 gen
 
+tahapan-tahapan dalam GA :
+1. Representasi Individu
+2. Evaluasi individu menggunakan fitness
+3. Seleksi Orangtua
+4. Rekombinasi penggunakan crossover
+5. Mutasi
+6. pergantian generasi
+
+penyeleksian dari orangtua menggunakan metode Roulette-wheel
+
 saat ini sudah mau memasuki tahap cross-over, cross-over disini menggunakan N-point cross-over yang memiliki 3 titik.
--- main.py
+++ main.py
@@ -13,18 +13,28 @@ def encoding(x,y): #encode decimal to binary
 	bin2 = "{0:04b}".format(y)
 	return bin1+bin2
 
-def fitnes(f):
+def fitnes(f): #fitness
 	return 1/(f+0.1)
 
-def fungsi(x,y):
+def fungsi(x,y): #problem
 	return 3*(x**2) + 2*(y**2) - 4*x + y/2
 
-def parentSelection(probPopulation, rand):
+def parentSelection(fitnessPopulation, rand): #seleksi orangtua
 	total = 0
 	for a in range(10):
-		total += probPopulation[a]
+		total += fitnessPopulation[a]
 		if(total > rand):
 			return a
+
+def crossOver(par): #fungsi crossover
+	titik = sample(range(1,8),3)
+	titik.sort()
+	silang = randint(0,1)
+	a = 0 if silang == 0 else 1
+	b = 1 if silang == 0 else 0
+	child1 = par[a][titik[0]:titik[1]] + par[a][titik[2]:] + par[b][:titik[0]] + par[b][titik[1]:titik[2]]
+	child2 = par[b][titik[0]:titik[1]] + par[b][titik[2]:] + par[a][:titik[0]] + par[a][titik[1]:titik[2]]
+	return [child1, child2]
 
 #inisialisasi populasi
 pop = [[randint(0,1) for x in range(gen)] for y in range(nPop)]
@@ -37,20 +47,12 @@ for a in range(10):
 	bil = decoding(conv)
 	genFit.append(fitnes(fungsi(bil[0], bil[1])))
 
-prob = [(genFit[a]/sum(genFit)) for a in range(10)] #probabilitas fitness 
-
 #seleksi orang tua
-par = []
-while len(par) < 2: #roulette-wheel
-	n = uniform(0,sum(genFit))
-	par.append(pop[parentSelection(prob,n)])
+parent = []
+while len(parent) < 2: #roulette-wheel
+	n = uniform(min(genFit),sum(genFit))
+	parent.append(pop[parentSelection(genFit,n)])
 
-# def crossOver(kromosom):
-# 	titik = sample(range(1,8),3)
-# 	# i = 0
-# 	# while len(titik) < 3 :
-# 	# 	titik.append(randint(1,8))
-# 	# 	titik.sort()
-# 	return titik.sort()
-
-# print crossOver(par[0])
+#crossover
+child = crossOver(parent)
+print child
